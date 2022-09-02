0432bdb9ecf626c21605e1c58bf8c329494ed2a3
JosephYu1 <Joseph_Yu1@baylor.edu>
2022-09-02 08:50:32 -0500

    file header for tool script

---
+++ SRA_Batch_Download.sh
@@ -0,0 +1,6 @@
+# File Name:    SRA_Batch_Download.sh
+# Description:  This will be the script for batch download from SRA using the SRA toolkit
+# Author:       Joseph Yu
+# Date Created: 9-2-2022
+# Project:      Rhesus_PrediXcan
+
