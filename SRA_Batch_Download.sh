d3c430c0612c87a8b535bb0cd800189a14b6271f
JosephYu1 <Joseph_Yu1@baylor.edu>
2022-09-06 16:28:51 -0500

    simple argument and file permission check

--- SRA_Batch_Download.sh
+++ SRA_Batch_Download.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
 # File Name:    SRA_Batch_Download.sh
 # Description:  This will be the script for batch download from SRA using the SRA toolkit
 # Author:       Joseph Yu
 # Date Created: 9-2-2022
 # Project:      Rhesus_PrediXcan
 
+if [[ $# -ne 1 ]]
+then
+  echo "usage: SRA_Batch_Download.sh [ file ]" > /dev/stderr
+fi
+
+if ! [ -r $1 ]
+then
+  echo "$1 is not readable"
+fi
